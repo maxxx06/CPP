be6cf020c91fa3d4b4c5e39becb8c3dc5fa66d22
Maxime Lecomte <maxime.lecomte06@free.fr>
2019-03-05 11:19:57 +0100

    structure de base

--- Agence.hpp
+++ Agence.hpp
@@ -1,6 +1,13 @@
+#ifndef AGENCE_HPP
+#define AGENCE_HPP
+
 #include <iostream>
 using namespace std;
 
 class Agence {
 
-}
+private:
+    
+
+};
+#endif
--- Biens.hpp
+++ Biens.hpp
@@ -1,6 +1,15 @@
+#ifndef  BIENS_HPP
+#define BIENS_HPP
+
 #include <iostream>
 using namespace std;
 
 class Biens {
+private:
+    int prix;
+    std::string adresse_biens;
+    int ref_client;
+    int num;   
+};
 
-}
+#endif
