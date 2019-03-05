cbd7a85ac63e0495ff8fb9e8b87d5c2c59a3cc0b
Maxime Lecomte <maxime.lecomte06@free.fr>
2019-03-05 11:43:32 +0100

    dico

---
+++ Clients_acheteurs.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+#include "Clients_acheteurs.hpp"
--- Clients_acheteurs.hpp
+++ Clients_acheteurs.hpp
@@ -1,14 +1,18 @@
 #ifndef CLIENTS_ACHETEURS_HPP
 #define CLIENTS_ACHETEURS_HPP
+#include <map>
 
 class Clients_acheteurs {
 private:
-    std::string* liste_visite; // metrtre en dico {biens : nombre_visite}
+    int nb_visite;
+    map<std::string* liste_biens, int nb_visite> liste_visite; // metrtre en dico {biens : nombre_visite}
     bool proposition_achat;
     int prix_achat;
 
 public:
-    Clients_acheteurs(std::string* liste_visite )
+    Clients_acheteurs();
+    Clients_acheteurs(map liste_visite, bool proposition_achat);
+    ~Clients_acheteurs();
 };
 
 #endif
